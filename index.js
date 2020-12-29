fd843c7e4d0aa6697c9a68cc41948f5dc4e0a09d
hee <Michael1117@users.noreply.github.com>
2020-12-30 01:23:41 +0800

    封装之前

--- index.js
+++ index.js
@@ -3,5 +3,22 @@
 const program = require('commander')
 
 //program.version("1.0.0")
+
+// 查看版本号
 program.version(require('./package.json').version)
+
+// 增加自己的options
+program.option('-m --mic', 'a mic cli')
+program.option('-d --dest <dest>', 'a destination folder, 例如： -d /src/components' )
+program.option('-d --framework <framework>', 'your framework' )
+
+program.on('--help', function() {
+    console.log("")
+    console.log("Other:")
+    console.log("other options~")
+})
+
+
 program.parse(process.argv)
+
+console.log(program.dest)
--- readme.md
+++ readme.md
@@ -10,3 +10,14 @@ console.log("Michael node")
 npm link
 mic
 ```
+
+```
+mic --version
+```
+
+```
+program.option('-d --dest <dest>', 'a destination folder, 例如： -d /src/components' )
+
+console.log(program.dest)
+
+```
